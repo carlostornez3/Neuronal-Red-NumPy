22df3626ee4e3dfe7fdfc6764f5f75c4c84b7e98
Jose Tornez <carlostornez3@gmail.com>
2024-01-25 23:59:52 -0600

    Este es solo un checkpoint. Aun no acabo de comentar, solo llevo la mitad

--- network.py
+++ network.py
@@ -11,14 +11,14 @@ and omits many desirable features.
 
 #### Libraries
 # Standard library
-import random
+import random #Importa una libreria que permite tener numeros aleatorios entre muchas otras funcionalidades
 
 # Third-party libraries
-import numpy as np
+import numpy as np #Libreria que permite la introducción de matrices entre otras muchas funciones
 
-class Network(object):
+class Network(object): #Declaración de nuestra clase, desciende de la clase objeto mediante herencia
 
-    def __init__(self, sizes):
+    def __init__(self, sizes): #Declaración del constructor. Al instanciar un objeto de tipo network, se le debe pasar una lista
         """The list ``sizes`` contains the number of neurons in the
         respective layers of the network.  For example, if the list
         was [2, 3, 1] then it would be a three-layer network, with the
@@ -29,20 +29,20 @@ class Network(object):
         layer is assumed to be an input layer, and by convention we
         won't set any biases for those neurons, since biases are only
         ever used in computing the outputs from later layers."""
-        self.num_layers = len(sizes)
-        self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.num_layers = len(sizes)#Atributo de la clase, se le asigna el número de elementos de la lista que se pasa al instanciar un objeto de la clase
+        self.sizes = sizes #Atributo de la clase, se le asigna al atributo la lista que se pasa al instanciar la clase
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]#Creación de un atributo, el cual es una lista con los bais. Es generado apartir de números aleatorios. Es una lista de matrices, cada matriz tendrá "y" filas y 1 columna.
         self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
+                        for x, y in zip(sizes[:-1], sizes[1:])] #Atributo de la clase el cuál es igual a una lista de matrices, las cuales tendrá "y" filas y "x" columnas. x, y se obtienen de sizes, x toma todos los valores excepto el de la capa de salida, y toma todos excepto el de la capa de entrada. Los elementos de todas las matrices son numeros entre -1 y 1
 
-    def feedforward(self, a):
+    def feedforward(self, a): #Se define un método de la clase. Toma como parámetro la activación
         """Return the output of the network if ``a`` is input."""
-        for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
-        return a
+        for b, w in zip(self.biases, self.weights):#El zip "empaqueta" los elementos de los atributos biases y weights de forma 1 a 1 y le asigna dichos valores a "b" y "w"
+            a = sigmoid(np.dot(w, a)+b) #Le asigna a "a" el valor de la sigmoide realizando el producto punto de la matriz de pesos y de activación, y le añade el valor del bias
+        return a #Regresa el valor de "a"
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
+            test_data=None): #Definición de un atributo de la clase
         """Train the neural network using mini-batch stochastic
         gradient descent.  The ``training_data`` is a list of tuples
         ``(x, y)`` representing the training inputs and the desired
@@ -51,24 +51,24 @@ class Network(object):
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
         tracking progress, but slows things down substantially."""
-        if test_data:
-            test_data = list(test_data)
-            n_test = len(test_data)
+        if test_data: #Si se proporcionó el parámetro test_data al llamar a la función
+            test_data = list(test_data)#Se convierte a test data en una lista
+            n_test = len(test_data)#Se guarda el número de elementos en una variable
 
-        training_data = list(training_data)
-        n = len(training_data)
-        for j in range(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k+mini_batch_size]
+        training_data = list(training_data)#Se convierte a lista
+        n = len(training_data)#Se guarda el número de elementos de la lista
+        for j in range(epochs): #Se comienza a iterar acorde al número de épocas o ciclos
+            random.shuffle(training_data) #Mezcla de manera aleatoria los elementos de la lista training_data
+            mini_batches = [#Creación de la lista donde irán los minibatches para entrenar la red
+                training_data[k:k+mini_batch_size] #Se toma el elemento k y los "mini_batch_size"(tamaño del minibatch) elementos siguientes. Esto se hace varias veces pues se está iterando, desde 0 hasta n dando brincos acorde al tamaño del minibatch deseado.Como resultado se tiene listas de listas
                 for k in range(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print("Epoch {0}: {1} / {2}".format(
+            for mini_batch in mini_batches:#Se iteran los elementos de la lista minibatches
+                self.update_mini_batch(mini_batch, eta)#Se manda a llamar un método definido más abajo pasandole como parámetros un minibatch y la eta. Profe, olvidé el nombre que tiene este valor pero si sé que controla "el tamaño de cada paso" cuando intentamos minimizar la función de costo
+            if test_data: #Si se proporcionó test_data
+                print("Epoch {0}: {1} / {2}".format( #Imprime en la consola "Epoch (valor de j): (valor del método evaulate)/(número de pruebas o entrenamientos)
                     j, self.evaluate(test_data), n_test))
-            else:
-                print("Epoch {0} complete".format(j))
+            else: #Si no: 
+                print("Epoch {0} complete".format(j)) #Imprime en consola: "Epoca (valor de j) complete"
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying

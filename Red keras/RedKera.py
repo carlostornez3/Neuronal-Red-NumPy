c9f2a404aed89d88264f0802c81318082f618125
Jose Tornez <carlostornez3@gmail.com>
2024-02-12 18:16:38 -0600

    Se redujo el número de datos y se aumentó el número de epocas para ocasionar overfitting

--- Red keras/RedKera.py
+++ Red keras/RedKera.py
@@ -5,14 +5,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import RMSprop, SGD, Adam
 from tensorflow.keras import activations
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import time
+import matplotlib.pyplot as plt
 
 #Para escribir logs en wandb
 #pip install wandb
 #wandb login
 
 learning_rate = 0.001
-epochs = 30
+epochs = 300
 batch_size = 120
 
 #import wandb
@@ -31,9 +33,14 @@ dataset=mnist.load_data()
 
 
 (x_train, y_train), (x_test, y_test) = dataset
-
-x_trainv = x_train.reshape(60000, 784)
-x_testv = x_test.reshape(10000, 784)
+data_number=10000
+x_train = x_train[:data_number]
+y_train = y_train[:data_number]
+x_test = x_test[:data_number]
+y_test = y_test[:data_number]
+print(x_train.shape)
+x_trainv = x_train.reshape(data_number, 784)
+x_testv = x_test.reshape(data_number, 784)
 
 x_trainv = x_trainv.astype('float32')
 x_testv = x_testv.astype('float32')
@@ -58,6 +65,7 @@ model.add(Dense(20, activation='sigmoid'))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
+earlystop = EarlyStopping(monitor='val_loss',mode='min',restore_best_weights=False,patience=20,verbose=1)
 start_time = time.time()
 model.compile(loss='binary_crossentropy',optimizer=Adam(learning_rate=learning_rate,beta_1=0.9,beta_2=0.99, epsilon=10e-09),metrics=['accuracy'])
 
@@ -71,7 +79,14 @@ history = model.fit(x_trainv, y_trainc,
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"El tiempo de ejecución fue de {execution_time} segundos.")
-
+plt.plot(history.history['loss'],label="Loss")
+plt.plot(history.history['val_loss'],label="Val_Loss")
+plt.legend()
+plt.figure()
+plt.plot(history.history['accuracy'], label="Accuracy")
+plt.plot(history.history['val_accuracy'], label="val_Accuracy")
+plt.legend()
+plt.show()
 
 score = model.evaluate(x_testv, y_testc, verbose=1)
 
